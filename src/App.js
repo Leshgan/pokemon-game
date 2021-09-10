7758c1e83094414e2238ee771c8ccb1569f08af2
Leshgan <leshgans@gmail.com>
2021-09-10 11:21:15 +0300

    New layout (#1)
    * new layout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import logo from './logo.svg';
+import Header from './components/Header';
+import Layout from './components/Layout';
+import Footer from './components/Footer';
 import './App.css';
+import bg1 from './assets/images/bg1.jpeg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-      </header>
-    </div>
+    <>
+      <Header title="Header title" descr="Description of Header" />
+      <Layout
+        title="First title"
+        descr="first description"
+        urlBg={bg1} />
+      <Layout
+        title="Second title"
+        descr="second description"
+        colorBg="cyan" />
+      <Layout
+        title="Third title"
+        descr="third description"
+        urlBg={bg1} />
+      <Footer />
+    </>
   );
 }
 
